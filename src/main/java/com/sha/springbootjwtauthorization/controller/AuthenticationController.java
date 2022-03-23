f40438e5aa1c9f46e3aff97a2f56034e5e3fe757
johnpaulap <johnthepop@gmail.com>
2022-03-23 22:45:46 +0400

    UserController added
    Error on calling localhost:8080/api/user/change/ADMIN

--- src/main/java/com/sha/springbootjwtauthorization/controller/AuthenticationController.java
+++ src/main/java/com/sha/springbootjwtauthorization/controller/AuthenticationController.java
@@ -7,10 +7,7 @@ import com.sha.springbootjwtauthorization.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -38,7 +35,7 @@ public class AuthenticationController {
             user.setCreateTime(LocalDateTime.now());
         }
         userService.saveUser(user);
-        return new ResponseEntity<>("Hello World",HttpStatus.CREATED);
+        return new ResponseEntity<User>(user,HttpStatus.CREATED);
     }
 
     @PostMapping("sign-in")
@@ -48,7 +45,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("refresh-token")
-    public ResponseEntity<?> refreshToken(@RequestBody String token)
+    public ResponseEntity<?> refreshToken(@RequestParam String token)
     {
         return ResponseEntity.ok(jwtTokenRefreshService.generateAccessTokenFromRefreshToken(token));
     }
